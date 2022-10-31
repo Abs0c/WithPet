b2976ea15939e9107290f1ab4a883cfebe92b3b2
PAJUYO <parkju1226@gmail.com>
2022-10-31 19:19:10 +0900

    10/31 캘린더부분과 식단계산기 부분업로드 추후 데이터베이스부분 올릴예정

--- app/src/main/java/com/example/porject/Diary.kt
+++ app/src/main/java/com/example/porject/Diary.kt
@@ -1,15 +1,29 @@
 package com.example.porject
 
+import android.R
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.example.porject.databinding.FragmentDiaryBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,25 +31,88 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Diary : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+
+    var userID: String = "userID"
+    lateinit var fname: String
+    lateinit var str: String
+    lateinit var calendarView: CalendarView
+    lateinit var readBtn: Button
+    lateinit var diaryTextView: TextView
+    lateinit var diaryContent: TextView
+    lateinit var title: TextView
+    lateinit var binding: FragmentDiaryBinding;
+
+    private lateinit var auth: FirebaseAuth
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        binding = FragmentDiaryBinding.inflate(layoutInflater)
+        calendarView = binding.calendarView
+        diaryTextView = binding.diaryTextView
+        readBtn = binding.readBtn
+        diaryContent = binding.diaryContent
+        title = binding.title
+        title.text = "달력 일기장"
+
+
+
+
+
+        //펫리스트 일단 예시
+        val items = arrayOf<String>("뽀삐", "퉁퉁이")
+
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.visibility = View.VISIBLE
+            readBtn.visibility = View.VISIBLE
+
+            diaryContent.visibility = View.INVISIBLE
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+
+        }
+
+        val auth1 = Firebase.auth.currentUser
+        binding.readBtn.setOnClickListener() {
+            var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+            AlertDialog.Builder(requireContext()).run {
+                setTitle("펫목록")
+                setIcon(R.drawable.ic_dialog_info)
+                setItems(items, object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        Log.d("kkang", "선택강아지:${items[p1]}")
+
+                    }
+                })
+                setPositiveButton("닫기", null)
+                show()
+
+            }
+        }
+
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_diary, container, false)
     }
+
+
 
     companion object {
         /**
@@ -50,10 +127,10 @@ class Diary : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Diary().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
             }
     }
 }
--- app/src/main/java/com/example/porject/Diet.kt
+++ app/src/main/java/com/example/porject/Diet.kt
@@ -1,16 +1,150 @@
 package com.example.porject
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.porject.databinding.ActivityDietBinding
 
-class Diet : AppCompatActivity() {
-    lateinit var binding: com.example.porject.databinding.ActivityDietBinding
 
+
+class   Diet : AppCompatActivity() {
+    lateinit var binding: ActivityDietBinding
+    private var shame:Double=0.0
+    private var editdata:Double=0.0
+
+    private fun setListenerToEditText() {
+        binding.weightWrite.setOnKeyListener { view, keyCode, event ->
+            // Enter Key Action
+            if (event.action == KeyEvent.ACTION_DOWN
+                && keyCode == KeyEvent.KEYCODE_ENTER
+            ) {
+                // 키패드 내리기
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.weightWrite.windowToken, 0)
+                // Toast Message
+                true
+            }
+            false
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDietBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
+
+
+        val spinner = findViewById<Spinner>(R.id.dog_situation)
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.my_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
+
+            override fun onNothingSelected(parent: AdapterView<*>?){
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position){
+                    //선택하지 않은경우
+                    0 -> {
+                        shame = 0.0
+                    }
+                    //성장기(4~12개월)
+                    1 -> {
+                        //수치3
+                        shame = 3.0
+                    }
+                    //성장기(4~12개월)
+                    2 -> {
+                        //수치2
+                        shame = 2.0
+                    }
+                    //미중성 성견
+                    3 -> {
+                        //수치1.8
+                        shame = 1.8
+                    }
+                    //중성화 완료 성견
+                    4 -> {
+                        //수치1.6
+                        shame = 1.6
+                    }
+                    //과체중 성견
+                    5 -> {
+                        //수치1.4
+                        shame = 1.4
+                    }
+                    //비만인 성견
+                    6 -> {
+                        //수치 1
+                        shame = 1.0
+                    }
+                    //임산견 (임신전반 42일간)
+                    7 -> {
+                        //수치1.8
+                        shame = 1.8
+                    }
+
+                    //임산견 (임신후반 21일간)
+                    8 -> {
+                        //3
+
+                        shame = 3.0
+                    }
+                    //노령견
+                    9 -> {
+                        //1.4
+                        shame = 1.4
+
+                    }
+
+                    //일치하는게 없는 경우
+                    else -> {
+
+                    }
+
+                }
+            }
+        }
+
+        val sumBtn : Button= findViewById(R.id.sumBtn)
+
+        sumBtn.setOnClickListener {
+            var pet_kcal:Double = 0.0
+
+            var ses=binding.weightWrite.text.toString()
+            editdata=ses.toDouble()
+            //강아지 몸무게가 2kg이상일시
+            if (editdata>=2.0) {
+                pet_kcal = (30*editdata+70) * shame
+                binding.calculation.text=pet_kcal.toString()
+            }
+            //강아지 몸무게가 2kg미만일시
+            else if(editdata<2.0){
+                pet_kcal = (70 * editdata * (0.75)) * shame
+                binding.calculation.text=pet_kcal.toString()
+            }
+            else{
+
+            }
+        }
 
 
     }
