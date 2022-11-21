f93959d6332965c800d4b2a87ec54cf1e22473fa
Abs0c <qjfjrdl12@naver.com>
2022-11-21 20:54:20 +0900

    mypage의 버튼 기능추가 및 수정

--- app/src/main/java/com/example/porject/myPage.kt
+++ app/src/main/java/com/example/porject/myPage.kt
@@ -1,16 +1,24 @@
 package com.example.porject
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.porject.databinding.FragmentMyPageBinding
 
 class myPage : Fragment(), View.OnClickListener {
     lateinit var binding: FragmentMyPageBinding
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+        ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,14 +40,29 @@ class myPage : Fragment(), View.OnClickListener {
             startActivity(intent)
         }
         binding.btnSettingFrag.setOnClickListener{
-            Toast.makeText(activity, "미구현된 기능입니다.", Toast.LENGTH_SHORT).show()
+            var permissioncheck: Boolean = true
+            for (permission in REQUIRED_PERMISSIONS){
+                if (ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_DENIED){
+                    permissioncheck = false
+                    break
+                }
+            }
+            when (permissioncheck) {
+                false -> {
+                    requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
+                }
+                true -> {
+                    Toast.makeText(activity, "권한 설정이 이미 완료되어 있습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        //binding.btnInfo.setOnClickListener{
+            //Toast.makeText(activity, "미구현된 기능입니다.", Toast.LENGTH_SHORT).show()
+        //}
 
-        }
-        binding.btnInfo.setOnClickListener{
-            Toast.makeText(activity, "미구현된 기능입니다.", Toast.LENGTH_SHORT).show()
-        }
         binding.btnHelp.setOnClickListener{
-            Toast.makeText(activity, "미구현된 기능입니다.", Toast.LENGTH_SHORT).show()
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://open.kakao.com/o/gLN28fPe"))
+            startActivity(intent)
         }
 
         return binding.root
